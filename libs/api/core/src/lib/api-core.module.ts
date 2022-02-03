291c23dd840b5255425e149ec7e99fb629f4de2a
Carlos Aquino <carlos.aquino.ext@infracommerce.com.br>
2022-02-03 14:53:39 -0300

    docker simples-api postgre - working

--- libs/api/core/src/lib/api-core.module.ts
+++ libs/api/core/src/lib/api-core.module.ts
@@ -1,8 +1,38 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
+import { configuration } from './config/configuration'
+import { DatabaseConfig } from './config/db.config'
+import { validationSchema } from './config/validation'
+
+const ENV = process.env.NODE_ENV
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      isGlobal: true,
+      load: [configuration],
+      validationSchema,
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+      playground: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: DatabaseConfig,
+    }),
+  ],
   controllers: [],
   providers: [],
   exports: [],
 })
-export class ApiCoreModule {}
+export class ApiCoreModule {
+  constructor() {
+    console.log('AMBIENTE', ENV, !ENV ? '.env' : `.env.${ENV}`)
+    console.log('ENV_FILE', process.env.ENV_FILE)
+    console.log(process.env)
+  }
+}
